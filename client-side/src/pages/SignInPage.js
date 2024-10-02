1036074dcfd0fc124e4c5553dd4f10c438d642af
duong <22520298@gm.uit.edu.vn>
2024-10-02 19:12:52 +0700

    LoginPage

--- client-side/src/pages/SignInPage.js
+++ client-side/src/pages/SignInPage.js
@@ -1,3 +1,45 @@
+import Forget from "~/component/Form/ForgetForm";
+import LoginForm from "~/component/Form/LoginForm";
+import RegisterForm from "~/component/Form/RegisterForm";
+import React, { useState } from 'react';
+import Carousel from "~/component/Form/Carousel";
+
+
+
+
 export default function SignInPage() {
-    return <h1>SignIn Page</h1>;
+    const [signUpMode, setSignUpMode] = useState(false);
+  const [showForgetForm, setShowForgetForm] = useState(false); // Trạng thái hiển thị ForgetForm
+
+  const toggleMode = () => {
+    setSignUpMode(!signUpMode);
+  };
+
+  const handleShowForgetForm = () => {
+    setShowForgetForm(true); // Mở ForgetForm
+  };
+
+  const handleCloseForgetForm = () => {
+    setShowForgetForm(false); // Đóng ForgetForm
+  };
+
+  return (
+    <main className={signUpMode ? 'sign-up-mode' : ''}>
+      <div className="box">
+        <div className="inner-box">
+          <div className="forms-wrap">
+            {!signUpMode ? (
+              <LoginForm toggleMode={toggleMode} onGetHelpClick={handleShowForgetForm} /> // Truyền hàm để mở ForgetForm
+            ) : (
+              <RegisterForm toggleMode={toggleMode} />
+            )}
+          </div>
+          <Carousel />
+
+          {/* Pop-up Forget Password */}
+          {showForgetForm && <Forget onClose={handleCloseForgetForm} />} 
+        </div>
+      </div>
+    </main>
+  );
 }
--- client-side/src/routes/index.js
+++ client-side/src/routes/index.js
@@ -4,12 +4,12 @@ import CartPage from '~/pages/CartPage';
 import PayingPage from '~/pages/PayingPage/PayingPage';
 import HomePage from '~/pages/HomePage/HomePage';
 import SignInPage from '~/pages/SignInPage';
-import SignUpPage from '~/pages/SignUpPage';
+
 
 const Pages = [
     { path: routes.home, Component: HomePage },
     { path: routes.signIn, Component: SignInPage },
-    { path: routes.signUp, Component: SignUpPage },
+
     { path: routes.cart, Component: CartPage },
     { path: routes.bookDetail, Component: BookDetail },
     { path: routes.paying, Component: PayingPage },
